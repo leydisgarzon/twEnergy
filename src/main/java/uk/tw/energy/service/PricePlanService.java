9a67ac3321245be26f135f3b1b21e6c29024eee0
Leydis Garzón Giro <leydis.garzon@gmail.com>
2021-01-21 13:49:11 -0500

    Using method reference to sum list values

--- src/main/java/uk/tw/energy/service/PricePlanService.java
+++ src/main/java/uk/tw/energy/service/PricePlanService.java
@@ -41,7 +41,7 @@ public class PricePlanService {
     private BigDecimal calculateAverageReading(List<ElectricityReading> electricityReadings) {
         BigDecimal summedReadings = electricityReadings.stream()
                 .map(ElectricityReading::getReading)
-                .reduce(BigDecimal.ZERO, (reading, accumulator) -> reading.add(accumulator));
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
 
         return summedReadings.divide(BigDecimal.valueOf(electricityReadings.size()), RoundingMode.HALF_UP);
     }
